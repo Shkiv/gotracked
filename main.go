73ea2193678bc78530bac664747d0c289d3121fa
Shkiv <romanovma@lenta.ru>
2021-11-21 15:59:04 +0200

    Add stop

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 
 	router := gin.Default()
 	router.GET("/start", start)
-	router.GET("/end", end)
+	router.GET("/stop", stop)
 	router.GET("/intervals", getIntervals)
 
 	router.Run("localhost:8080")
@@ -37,17 +37,47 @@ func start(context *gin.Context) {
 	rows.Scan(&count)
 	if count < 1 {
 		time := time.Now()
-		_, err := db.Exec("INSERT INTO active_intervals VALUES (?, NULL)", time)
+		_, err := db.Exec("INSERT INTO active_intervals (interval_start) VALUES (?)", time)
 		if err != nil {
-			context.JSON(http.StatusInternalServerError, err)
+			log.Print(err)
+			context.JSON(http.StatusInternalServerError, nil)
+			return
 		}
 		context.JSON(http.StatusCreated, "Created")
+		return
 	}
 	context.JSON(http.StatusOK, "Not changed")
 }
 
-func end(context *gin.Context) {
+func stop(context *gin.Context) {
+	var startTime sql.NullString
 
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanErr := tx.QueryRow("SELECT interval_start FROM active_intervals").Scan(&startTime)
+	if scanErr != nil {
+		tx.Rollback()
+		context.JSON(http.StatusOK, "No interval")
+		return
+	}
+	time := time.Now()
+	_, insErr := tx.Exec("INSERT INTO intervals VALUES (?, ?)", startTime, time)
+	if insErr != nil {
+		tx.Rollback()
+		context.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+	_, delErr := tx.Exec("DELETE FROM active_intervals")
+	if delErr != nil {
+		tx.Rollback()
+		context.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+	tx.Commit()
+
+	context.JSON(http.StatusOK, "OK")
 }
 
 func getIntervals(context *gin.Context) {
@@ -103,15 +133,14 @@ func CreateDb() {
 	}
 
 	_, err1 := db.Exec(`CREATE TABLE IF NOT EXISTS active_intervals(
-		interval_start timestamp with time zone,
-		interval_end timestamp with time zone)`)
+		interval_start timestamp with time zone)`)
 	if err1 != nil {
-		log.Fatal(err0)
+		log.Fatal(err1)
 	}
 }
 
 func initDb() {
-	dsn := "file:locked.sqlite?cache=shared&parseTime=true"
+	dsn := "file:gotracked.sqlite?cache=shared&parseTime=true"
 	var err error
 
 	// Opening a driver typically will not attempt to connect to the database.
